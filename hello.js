1b9eadc40dcf737377ee5bcecdb08645e8a2ef2e
ZerefRD <juanman.1305@gmail.com>
2024-05-21 12:18:10 -0400

    Updated the hello.js with the instructions provided

--- hello.js
+++ hello.js
@@ -4,7 +4,11 @@ Enter your code on line 7 to print 'Hello, World!'
 to the console (the panel on the right)
 */
 
-console.log("Hello");
+console.log('Hello LaunchCode')
+console.log(22+2)
+console.log("Hello"); console.log("world")
+console.log('"Nevermore"')
+console.log("Ready!")
 
 /*  
 Next, PLAY! 
